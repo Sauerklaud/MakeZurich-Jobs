4869111da01905654d86bd9d06a8902619ae7611
darior <dario.roethlisberger@outlook.com>
2017-02-03 17:42:12 +0100

    added some functionality to the worker

--- worker.py
+++ worker.py
@@ -1,9 +1,21 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
+from pymongo import MongoClient
 
 sched = BlockingScheduler()
 
 @sched.scheduled_job('interval', minutes=1)
 def timed_job():
-    print('This job is run every three minutes.')
+    client = MongoClient('mongodb://back:1234@ds035059.mlab.com:35059/heroku_mqq5pbhp')
+    db = client['heroku_mqq5pbhp']
+    collection = db['airquality']
+    results = collection.find()
 
+    noOfRecords = 0
+    for record in results:
+        noOfRecords = noOfRecords + 1
+        print(record['raw'])
+    print('Number of db-entries: ' + str(noOfRecords))
+
+    client.close()
+    
 sched.start()
