723372460d6c4a4ab2544d7d8775ac5727da2588
darior <dario.roethlisberger@outlook.com>
2017-02-03 20:23:16 +0100

    fixed json format

--- worker.py
+++ worker.py
@@ -32,7 +32,7 @@ pusher_client = pusher.Pusher(
 
 @sched.scheduled_job('interval', seconds=5)
 def timed_job():
-    pusher_client.trigger('my-channel', 'my-event', {'long': '47.401996', 'lat': '8.444278'})
+    pusher_client.trigger('my-channel', 'my-event', {long: '47.401996', lat: '8.444278'})
 
 
 sched.start()
