7de8bda785cd6e0ca3111efc3d0dfe59bd4aa8c7
darior <dario.roethlisberger@outlook.com>
2017-02-03 17:20:46 +0100

    added basic scheduling functionality

--- hello/views.py
+++ hello/views.py
@@ -2,8 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from pymongo import MongoClient
 from .models import Greeting
-
 import requests
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+sched = BlockingScheduler()
+
+@sched.scheduled_job('interval', minutes=1)
+def timed_job():
+    print('This job is run every three minutes.')
+
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
+def scheduled_job():
+    print('This job is run every weekday at 5pm.')
+
+sched.start()
+
 
 def index(request):
     client = MongoClient('mongodb://back:1234@ds035059.mlab.com:35059/heroku_mqq5pbhp')
