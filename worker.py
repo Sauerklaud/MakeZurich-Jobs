81d581abbd167a558093e21f459a50fa41873bba
darior <dario.roethlisberger@outlook.com>
2017-02-03 20:57:46 +0100

    updated message

--- worker.py
+++ worker.py
@@ -31,8 +31,15 @@ pusher_client = pusher.Pusher(
 
 @sched.scheduled_job('interval', seconds=5)
 def timed_job():
-    pusher_client.trigger('my-channel', 'my-event', {
-        'message': '47.401996,8.444278'
+    pusher_client.trigger('my-channel', 'my-event', 
+    {
+        "pm10": '342',
+        "pm25": '430',
+        "latitude": '47.5',
+        "longitude": '7.5',
+        "timestamp": '2343242',
+        "meas_id": '4342',
+        "isValid": 'true'
     })
 
 sched.start()
