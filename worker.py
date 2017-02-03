c6fb2de7d98f32e1d58ab77698d08fa949539df7
darior <dario.roethlisberger@outlook.com>
2017-02-03 21:15:02 +0100

    removed message

--- worker.py
+++ worker.py
@@ -32,7 +32,7 @@ pusher_client = pusher.Pusher(
 @sched.scheduled_job('interval', seconds=5)
 def timed_job():
     pusher_client.trigger('my-channel', 'my-event', 
-    {'message': {
+    {
         "pm10": '342',
         "pm25": '430',
         "latitude": '47.5',
@@ -40,6 +40,6 @@ def timed_job():
         "timestamp": '2343242',
         "meas_id": '4342',
         "isValid": 'true'
-    }})
+    })
 
 sched.start()
