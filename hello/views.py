6cdd3eccf5fbd7c9b064f9498988993dcbe3bba4
darior <dario.roethlisberger@outlook.com>
2017-02-03 16:14:35 +0100

    added mongodb functionality

--- hello/views.py
+++ hello/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from pymongo import MongoClient
 from .models import Greeting
 
 import requests
 
 def index(request):
-    r = requests.get('http://httpbin.org/status/418')
-    print r.text
+    #r = requests.get('http://httpbin.org/status/418')
+    #print r.text
+    
+    client = MongoClient('mongodb://back:1234@ds035059.mlab.com:35059/heroku_mqq5pbhp')
+    db = client['heroku_mqq5pbhp']
+    collection = db['airquality']
+    results = collection.find()
+
+    for record in results:
+        print(record['raw'])
+
     return HttpResponse('<pre>' + r.text + '</pre>')
 
 def db(request):
