ccbdad433d3b346a8552fe4a4d22865b45a30e29
darior <dario.roethlisberger@outlook.com>
2017-02-03 16:26:07 +0100

    display first row of db

--- hello/views.py
+++ hello/views.py
@@ -6,9 +6,6 @@ from .models import Greeting
 import requests
 
 def index(request):
-    r = requests.get('http://httpbin.org/status/418')
-    #print r.text
-    
     client = MongoClient('mongodb://back:1234@ds035059.mlab.com:35059/heroku_mqq5pbhp')
     db = client['heroku_mqq5pbhp']
     collection = db['airquality']
@@ -17,7 +14,7 @@ def index(request):
     for record in results:
         print(record['raw'])
 
-    return HttpResponse('<pre>' + r.text + '</pre>')
+    return HttpResponse('<pre>' + print results[0]['raw'] + '</pre>')
 
 def db(request):
 
