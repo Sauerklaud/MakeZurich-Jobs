9efb1c144eef01becb3365dd2bc052f5f5dfe6d2
darior <dario.roethlisberger@outlook.com>
2017-02-03 20:38:36 +0100

    afddsf

--- worker.py
+++ worker.py
@@ -4,26 +4,27 @@ import pusher
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=1)
-def timed_job():
-    client = MongoClient('mongodb://back:1234@ds035059.mlab.com:35059/heroku_mqq5pbhp')
-    db = client['heroku_mqq5pbhp']
-    collection = db['airquality']
-    results = collection.find()
+#@sched.scheduled_job('interval', minutes=1)
+#def timed_job():
+#    client = MongoClient('mongodb://back:1234@ds035059.mlab.com:35059/heroku_mqq5pbhp')
+#    db = client['heroku_mqq5pbhp']
+#    collection = db['airquality']
+#    results = collection.find()
 
-    noOfRecords = 0
-    for record in results:
-        noOfRecords = noOfRecords + 1
-        print(record['raw'])
-    print('Number of db-entries: ' + str(noOfRecords))
+#    noOfRecords = 0
+#    for record in results:
+#       noOfRecords = noOfRecords + 1
+#       print(record['raw'])
+#   print('Number of db-entries: ' + str(noOfRecords))
 
-    client.close()
+#    client.close()
 
 
 pusher_client = pusher.Pusher(app_id='298067', key='256b563cc59616398c15', secret='ad03aff71ebb5af9bacc', cluster='eu', ssl=True)
 
-@sched.scheduled_job('interval', seconds=5)
+@sched.scheduled_job('interval', minutes=1)
 def timed_job():
+    print('before sending')
     pusher_client.trigger('my-channel', 'my-event', {message : '47.401996,8.444278'})
     print('sent')
 
